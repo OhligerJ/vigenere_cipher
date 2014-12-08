8a35c6ddc815446103c429fa842a747bfd110cf6
Jon1213 <jon1213@gmail.com>
2014-12-08 10:21:31 -0800

    encode complete

--- src/vigenere.js
+++ src/vigenere.js
@@ -1,7 +1,27 @@
 var vigenere = {};
 
+vigenere.repeat = function(keyword, length){
+  var mod = keyword.length;
+  var arr = keyword.split('');
+  ret_str = '';
+  for (var i = 0; i < length; i++){
+    ret_str += ( arr[i % mod]);
+  }
+  return ret_str;
+};
+
 vigenere.encode = function(message, keyword) {
-}
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var cipher_str = vigenere.repeat(keyword, message.length); //lemonlemonle
+  var ret_str = '';
+
+  for (var i = 0; i < message.length; i++){
+    ret_str = ret_str.concat( alphabet[ (alphabet.indexOf(message[i]) + (alphabet.indexOf(cipher_str[i]))) % 26 ] );
+  }
+  return ret_str;
+};
+
+console.log(vigenere.encode('attackatdawn', 'lemon'));
 
 // vigenere.decode = function(message, keyword) {
 // }
